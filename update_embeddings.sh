aa843fe93117c1e9c4ddb3bcf5d087b938b3c74a
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2024-12-07 18:25:22 +0530

    added echo date to update scripts

--- update_embeddings.sh
+++ update_embeddings.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# 0. Echo date
+echo "Starting script at $(date)"
+
 # 1. Activate the conda environment
 source /home/$USER/miniforge3/bin/activate papermatch
 
 # 2. Run the Python script
 python /home/$USER/embed_arxiv_simpler/update_embeddings.py >> /home/$USER/embed_arxiv_simpler/update_embeddings.log 2>&1
+
+# 3. Echo date
+echo "Finished script at $(date)"
--- update_milvus.sh
+++ update_milvus.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 0. Echo date
+echo "Starting script at $(date)"
+
 # 1. Activate the conda environment
 source /home/$USER/miniforge3/bin/activate papermatch
 
@@ -8,3 +11,6 @@ python /home/$USER/embed_arxiv_simpler/prepare_milvus.py >> /home/$USER/embed_ar
 
 # 3. Restart the systemd service
 systemctl --user restart papermatch.service
+
+# 4. Echo date
+echo "Script finished at $(date)"
