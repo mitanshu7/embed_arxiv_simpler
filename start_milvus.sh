08cd8fef29494af697fa51257f4f1ec071e8bb21
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2024-12-07 08:08:41 +0530

    comment out restart always if using podman+systemd

--- start_milvus.sh
+++ start_milvus.sh
@@ -50,6 +50,7 @@ EOF
         --health-start-period=90s \
         --health-timeout=20s \
         --health-retries=3 \
+        --restart always \
         docker.io/milvusdb/milvus:v2.4.17 \
         milvus run standalone  1> /dev/null
 }
