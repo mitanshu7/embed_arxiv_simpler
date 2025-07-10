734e2ec99b8a8898e1915f7004e245ab2af43e59
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-07-10 12:37:26 +0530

    Update consolidate with splitting the whole dataset in equal parts. fix glob path for accurate dataset loading

--- consolidate.py
+++ consolidate.py
@@ -4,7 +4,11 @@ from glob import glob
 from datasets import load_dataset
 from huggingface_hub import snapshot_download  # To download and concatenate vector data
 
+# Whether to consolidat FLoat or Binary vectors
 FLOAT = False
+# Also, whether to split the consolidated file into parts
+# Set 0 for no sharding.
+num_parts = 0
 
 # When 100% of the data is float, use the float repo
 if FLOAT:
@@ -17,7 +21,7 @@ else:
 # Download the repo
 repo_type = "dataset"
 local_dir = repo_id
-allow_patterns = "*.parquet"
+allow_patterns = f"{repo_id}/data/*.parquet"
 
 # Download the repo
 snapshot_download(
@@ -31,7 +35,17 @@ snapshot_download(
 parquet_files = glob(f"{repo_id}/**/*.parquet", recursive=True)
 
 # Create a new dataset
-dataset = load_dataset("parquet", data_dir=repo_id)
+dataset = load_dataset("parquet", data_dir=f"{repo_id}/data/")
 
 # Save consolidated dataset
 dataset["train"].to_parquet(f"{repo_id}/consolidated.parquet")
+
+
+# Save consolidated dataset in parts
+if num_parts:
+    print(f"Splitting consolidated dataset in {num_parts} parts.")
+
+    for i in range(num_parts):
+        dataset["train"].shard(num_parts, index=i).to_parquet(
+            f"{repo_id}/part_{i + 1}.parquet"
+        )
