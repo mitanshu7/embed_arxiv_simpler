63066f9f655e62d4ac0390b2de1c1a316a2c7046
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2026-08-10 17:45:04 +0530

    Update update_embeddings.sh

--- update_embeddings.sh
+++ update_embeddings.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # 0. Echo date
 echo "Starting script at $(date)"
-USER=$(whoami)
 
-# 1. Activate the conda environment
-source /home/$USER/miniforge3/bin/activate papermatch
+# 1. Activate the python environment
+source .venv/bin/activate
 
 # 2. Run the Python script
-python /home/$USER/embed_arxiv_simpler/update_embeddings.py >> /home/$USER/embed_arxiv_simpler/update_embeddings.log 2>&1
+python update_embeddings.py >> update_embeddings.log 2>&1
+python update_zilliz.py >> update_zilliz.log 2>&1
 
 # 3. Echo date
 echo "Finished script at $(date)"
