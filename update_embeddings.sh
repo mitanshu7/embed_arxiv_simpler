474c32ff85173ba6bd5c627313e367aaa69f8c1b
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2026-09-13 18:34:26 +0530

    overwrite the previous logs instead of piling

--- update_embeddings.sh
+++ update_embeddings.sh
@@ -9,8 +9,8 @@ echo "Starting script at $(date)"
 source .venv/bin/activate
 
 # 2. Run the Python script
-python update_embeddings.py >> update_embeddings.log 2>&1
-python update_zilliz.py >> update_zilliz.log 2>&1
+python update_embeddings.py > update_embeddings.log 2>&1
+python update_zilliz.py > update_zilliz.log 2>&1
 
 # 3. Echo date
 echo "Finished script at $(date)"
